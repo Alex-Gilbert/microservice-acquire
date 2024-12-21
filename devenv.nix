8eea6a22654404ec97e10bc3e5416ce7698bd081
Alex Gilbert <alexp.gilbert@pm.me>
2024-12-20 18:34:25 -0600

    Added Devenv and Setup Postgres

---
+++ devenv.nix
@@ -0,0 +1,28 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+
+# PostgreSQL service configuration
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    initialDatabases = [
+      {
+        name = "auth_db";
+        user = "auth_user";
+        pass = "auth_password";
+        schema = ./auth-service/schema.sql;
+      }
+    ];
+    port = 5432;  # Default port for PostgreSQL
+    listen_addresses = "127.0.0.1";  # Restrict access to localhost
+    settings = {
+      log_connections = true;  # Log each connection
+      log_statement = "all";  # Log all SQL statements
+      max_connections = 100;  # Allow up to 100 concurrent connections
+    };
+  };
+}
